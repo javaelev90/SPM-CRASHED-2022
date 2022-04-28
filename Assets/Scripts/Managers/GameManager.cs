c282d0f307ce42c2a0f8e800a1f48dd0d58f0a04
Anders <37899614+javaelev90@users.noreply.github.com>
2022-04-28 22:46:33 +0200

    Changed some game scripts logic

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,39 +6,37 @@ using Photon.Pun;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] PhotonObjectPool objectPool;
-    List<PooledObject> pooledObjects;
-    float timeDelay = 0.5f;
-    float timer = 0f;
+    [SerializeField] private GameObject objectPool;
+    [SerializeField] private GameObject characterPrefab;
+    [SerializeField] private Transform spawnPoint;
+    private bool IsMine { get { return photonView.IsMine; } }
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
     void Start()
     {
         Debug.Log($"Oh no, you chose the {(Character)PlayerPrefs.GetInt(GlobalSettings.GameSettings.CharacterChoicePropertyName)} charater");
     }
 
+    private void Initialize()
+    {
+
+        PhotonNetwork.Instantiate("Prefabs/" + characterPrefab.name, spawnPoint.position, spawnPoint.rotation);
+
+        if(PhotonNetwork.IsMasterClient)
+        {
+            //PhotonNetwork.InstantiateRoomObject("Prefabs/" + objectPool.name, Vector3.zero, Quaternion.identity);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            timer += Time.deltaTime;
-            if (Keyboard.current.rKey.wasPressedThisFrame && timer > timeDelay)
-            {
-                float x = Random.Range(1, 5);
-                float z = Random.Range(1, 5);
-                objectPool.Spawn(new Vector3(x, 1f, z));
-                timer = 0;
-            }
-            if (Keyboard.current.tKey.wasPressedThisFrame && timer > timeDelay)
-            {
-                pooledObjects = new List<PooledObject>(FindObjectsOfType<PooledObject>());
-                pooledObjects.RemoveAll(pooledObject => !pooledObject.gameObject.activeSelf);
-                
-                if (pooledObjects.Count > 0)
-                {
-                    objectPool.DeSpawn(pooledObjects[0].photonView.ViewID);
-                }
-                timer = 0;
-            }
+            
         }
        
     }
