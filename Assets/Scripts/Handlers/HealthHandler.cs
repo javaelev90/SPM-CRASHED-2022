50f79a6a2ff665154f9925311b8d97ee649c19b3
Anders <37899614+javaelev90@users.noreply.github.com>
2022-05-04 14:34:41 +0200

    fixed healthhandler bug

--- Assets/Scripts/Handlers/HealthHandler.cs
+++ Assets/Scripts/Handlers/HealthHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class HealthHandlerTest : MonoBehaviourPunCallbacks
+public class HealthHandler : MonoBehaviourPunCallbacks
 {
 
     public int MaxHealth { get; internal set; }
