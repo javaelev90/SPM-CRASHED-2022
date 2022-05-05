c30a9d6a3f5e19caeebc4bb765afbca673db165d
Anders <37899614+javaelev90@users.noreply.github.com>
2022-05-05 17:33:55 +0200

    Added punch to soldier

--- Assets/Scripts/3D/SoldierCharacter.cs
+++ Assets/Scripts/3D/SoldierCharacter.cs
@@ -10,6 +10,14 @@ public class SoldierCharacter : Controller3D
     [SerializeField] private float interactionDistance = 1f;
     [SerializeField] private Weapon weapon;
 
+    [Header("Punch settings")]
+    [SerializeField] float punchRange = 2f;
+    [SerializeField] int punchDamage = 1;
+    [SerializeField] float delayBetweenShots = 0.3f;
+    [SerializeField] LayerMask layersThatShouldBeHit;
+
+    private float shotCooldown = 0f;
+
     protected override void Awake()
     {
         base.Awake();
@@ -20,11 +28,52 @@ public class SoldierCharacter : Controller3D
     {
         base.Update();
         WeaponRotation();
+        Cooldown();
     }
 
     public void Shoot()
     {
         weapon.Shoot();
+    }
+
+    private void Cooldown()
+    {
+        if (OnCoolDown() == true)
+        {
+            shotCooldown -= Time.deltaTime;
+        }
+    }
+
+    private bool OnCoolDown()
+    {
+        return shotCooldown >= 0;
+    }
+
+    public void Punch()
+    {
+        if (OnCoolDown() == false)
+        {
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward,
+                out RaycastHit hitInfo, punchRange, layersThatShouldBeHit))
+            {
+                HealthHandler healthHandler = hitInfo.transform.GetComponent<HealthHandler>();
+                Debug.Log("Hit the enemy?");
+                if (healthHandler)
+                {
+                    Debug.Log("Hit the enemy.");
+                    healthHandler.TakeDamage(punchDamage);
+                }
+
+                AIBaseLogic ai = hitInfo.transform.GetComponent<AIBaseLogic>();
+                if (ai)
+                {
+                    Debug.Log(ai.transform.name);
+                    ai.FindAttackingTarget(transform);
+                }
+            }
+            // Add cooldown time
+            shotCooldown = delayBetweenShots;
+        }
     }
 
 }
