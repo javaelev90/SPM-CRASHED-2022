1f185877cf17f0545a77c06927a5c5f1ff7e2ca1
Simon Täcklind <simon.tacklind@gmail.com>
2022-05-05 21:57:31 +0200

    small fixes

--- Assets/Scripts/UI/ShowUI.cs
+++ Assets/Scripts/UI/ShowUI.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ShowUI : MonoBehaviour
 {
     public GameObject uiObject;
+    [SerializeField] private bool canSoldierPickup;
+    [SerializeField] private bool canEngineerPickup;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -13,17 +16,16 @@ public class ShowUI : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider player){
-        if(player.gameObject.tag == "Player")
+        if((canSoldierPickup && player.gameObject.GetComponent<SoldierCharacter>()) || (canEngineerPickup && player.gameObject.GetComponent<SoldierCharacter>()))
         {
            uiObject.SetActive(true);
         }
-        
     }
+
     private void OnTriggerExit(Collider player) {
-        if(player.gameObject.tag == "Player")
+        if(uiObject.activeSelf)
         {
            uiObject.SetActive(false);
-            
         }
     }
 }
