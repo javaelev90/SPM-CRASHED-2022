9684a83475d2927959053b561c30ec24d154fc3a
gretgurt <gretahassler@hotmail.com>
2022-05-10 12:13:04 +0200

    I put dissolve script and shader back mannen

---
+++ Assets/GretasStuff/Scripts/TestingDissolveShader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingDissolveShader : MonoBehaviour
+{
+
+    public Material dissolveMat;
+    public float health;
+    public float maxHealth;
+
+
+    void Start()
+    {
+        dissolveMat.SetFloat("DISSOLVEVALUE", health / maxHealth);
+    }
+
+    void Update()
+    {
+            health -= 0.1f;
+            dissolveMat.SetFloat("DISSOLVEVALUE", health / maxHealth);
+    }
+
+ 
+}
+
