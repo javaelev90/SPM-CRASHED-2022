cedc45adbaab182f246831f2733ad2fbb21a503d
Ossian Däckfors <ossian.dackfors@gmail.com>
2022-05-12 15:46:57 +0200

    Ship

---
+++ Assets/Resources/Prefabs/Ship/ShipHealth.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipHealth : HealthHandler
+{
+
+    public override void TakeDamage(int amount)
+    {
+        photonView.RPC(nameof(TakeDamageRPC), RpcTarget.All, amount);
+    }
+
+    [PunRPC]
+    private void TakeDamageRPC(int amount)
+    {
+        if (isAlive == true)
+        {
+            if (photonView.IsMine)
+            {
+                RemoveHealth(amount);
+            }
+        }
+    }
+}
