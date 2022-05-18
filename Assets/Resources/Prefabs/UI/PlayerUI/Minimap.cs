40fd1a6b5918e248c0a8dd4258bb91d754305c3f
ishohois <47395805+ishohois@users.noreply.github.com>
2022-05-18 09:23:02 +0200

    minimap update

--- Assets/Resources/Prefabs/UI/PlayerUI/Minimap.cs
+++ Assets/Resources/Prefabs/UI/PlayerUI/Minimap.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Minimap : MonoBehaviour
 {
-    public GameObject Player { get; set;}
-    public GameObject OtherPlayer { get; set;}
+    public GameObject Player { get; set; }
+    public GameObject OtherPlayer { get; set; }
     public GameObject Ship { get; set; }
 
+    [SerializeField] private RectTransform playerMarker;
     [SerializeField] private RectTransform shipMarker;
     [SerializeField] private RectTransform otherPlayerMarker;
+    [SerializeField] private RectTransform minimapCircle;
+    [SerializeField] private RectTransform outerMarkerShip;
+    [SerializeField] private RectTransform outerMarkerOtherPlayer;
+    [SerializeField] private float scale = 1f;
+    private float radius;
+    private GameObject outerMarkerShipParent;
 
     Vector2 shipMarkerPos;
     Vector2 otherPlayerPos;
@@ -23,6 +31,9 @@ public class Minimap : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+
+        radius = minimapCircle.sizeDelta.x / 2f;
+        outerMarkerShipParent = outerMarkerShip.transform.parent.gameObject;
     }
 
     // Update is called once per frame
@@ -32,19 +43,42 @@ public class Minimap : MonoBehaviour
         {
             if (Ship)
             {
-                Vector3 directionToShip = Player.transform.InverseTransformPoint(Ship.transform.position);
-                shipMarkerPos.x = directionToShip.x;
-                shipMarkerPos.y = directionToShip.z;
-                shipMarker.anchoredPosition = shipMarkerPos * 1f;
+                PositionOnMinimap(Ship.transform, shipMarker, shipMarkerPos);
+
+                if (!IsInsideUnitCircle(shipMarker))
+                {
+                    shipMarker.GetComponent<RawImage>().enabled = false;
+                    outerMarkerShip.gameObject.SetActive(true);
+                    float angle = Vector2.Angle(playerMarker.anchoredPosition, shipMarker.anchoredPosition);
+                    Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
+                    outerMarkerShipParent.transform.rotation = Quaternion.Slerp(outerMarkerShipParent.transform.rotation, rotation, 0.3f);
+                }
+                else
+                {
+                    outerMarkerShip.gameObject.SetActive(false);
+                    shipMarker.GetComponent<RawImage>().enabled = true;
+                }
             }
+
 
             if (OtherPlayer)
             {
-                Vector3 directionToOtherPlayer = Player.transform.InverseTransformPoint(OtherPlayer.transform.position);
-                otherPlayerPos.x = directionToOtherPlayer.x;
-                otherPlayerPos.y = directionToOtherPlayer.z;
-                otherPlayerMarker.anchoredPosition = otherPlayerPos * 1f;
+                PositionOnMinimap(OtherPlayer.transform, otherPlayerMarker, otherPlayerPos);
             }
         }
+
+    }
+
+    private bool IsInsideUnitCircle(RectTransform rect)
+    {
+        return Mathf.Pow(rect.anchoredPosition.x / radius, 2f) + Mathf.Pow(rect.anchoredPosition.y / radius, 2f) < 1f;
+    }
+
+    private void PositionOnMinimap(Transform objectToShow, RectTransform objectToShowMarker, Vector2 positionHolder)
+    {
+        Vector3 shipPosition = Player.transform.InverseTransformPoint(objectToShow.position);
+        positionHolder.x = shipPosition.x;
+        positionHolder.y = shipPosition.z;
+        objectToShowMarker.anchoredPosition = positionHolder * scale;
     }
 }
