6df860280d04f79a7e2df21b509e9ddcdb310ffd
ishohois <47395805+ishohois@users.noreply.github.com>
2022-05-18 14:06:23 +0200

    minimap

--- Assets/Resources/Prefabs/UI/PlayerUI/Minimap.cs
+++ Assets/Resources/Prefabs/UI/PlayerUI/Minimap.cs
@@ -16,6 +16,7 @@ public class Minimap : MonoBehaviour
     [SerializeField] private RectTransform outerMarkerShip;
     [SerializeField] private RectTransform outerMarkerOtherPlayer;
     [SerializeField] private Transform outerMarkerShipParent;
+    [SerializeField] private Transform outerMarkerOtherPlayerParent;
     [SerializeField] private float scale = 1f;
     private float radius;
 
@@ -44,30 +45,35 @@ public class Minimap : MonoBehaviour
             {
                 PositionOnMinimap(Ship.transform, shipMarker, shipMarkerPos);
 
-                if (!IsInsideUnitCircle(shipMarker))
-                {
-                    shipMarker.GetComponent<RawImage>().enabled = false;
-                    outerMarkerShip.gameObject.SetActive(true);
-
-                    Vector2 direction = shipMarker.anchoredPosition - Vector2.up;
-                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                    outerMarkerShipParent.transform.rotation = Quaternion.Euler(0f, 0f, angle);
-                }
-                else
-                {
-                    outerMarkerShip.gameObject.SetActive(false);
-                    shipMarker.GetComponent<RawImage>().enabled = true;
-                }
+                ActivateOuterMarker(shipMarker, outerMarkerShipParent);
             }
 
 
             if (OtherPlayer)
             {
                 PositionOnMinimap(OtherPlayer.transform, otherPlayerMarker, otherPlayerPos);
+                ActivateOuterMarker(otherPlayerMarker, outerMarkerOtherPlayerParent);
             }
         }
         
         Debug.Log("Angle " + angle);
+    }
+
+    private void ActivateOuterMarker(RectTransform objectMarker, Transform parent)
+    {
+        if (!IsInsideUnitCircle(objectMarker))
+        {
+            objectMarker.GetComponent<RawImage>().enabled = false;
+            parent.gameObject.SetActive(true);
+            Vector2 direction = objectMarker.anchoredPosition - playerMarker.anchoredPosition;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            parent.transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
+        }
+        else
+        {
+            parent.gameObject.SetActive(false);
+            objectMarker.GetComponent<RawImage>().enabled = true;
+        }
     }
 
     private bool IsInsideUnitCircle(RectTransform rect)
