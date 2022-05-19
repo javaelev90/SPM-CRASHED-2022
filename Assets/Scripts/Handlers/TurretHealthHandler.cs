965ff981df49ccb60943601e8371f5c43f24c591
HannaFrida <expresen.gameing@gmail.com>
2022-05-19 12:09:15 +0200

    Turret Drops more items

--- Assets/Scripts/Handlers/TurretHealthHandler.cs
+++ Assets/Scripts/Handlers/TurretHealthHandler.cs
@@ -68,6 +68,27 @@ public class TurretHealthHandler : HealthHandler
     [PunRPC]
     private void SpawnGreenGooRPC()
     {
-        InstantiateRoomObject(null);
+        Spawn();
+    }
+
+
+    public GameObject[] pickupPrefabs; //size gets set in inspector! drag prefabs in there!
+    public GameObject[] pickups;
+    public int amountToSpawn = 5;
+    [SerializeField] private float offsetY = 1f;
+
+    protected void Spawn()
+    {
+        pickups = new GameObject[pickupPrefabs.Length]; //makes sure they match length
+
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            for (int y = 0; y < pickupPrefabs.Length; y++)
+            {
+                Vector3 position = new Vector3(transform.position.x + Random.Range(-2, 2), transform.position.y + offsetY, transform.position.z + Random.Range(-3, 3));
+                pickups[y] = PhotonNetwork.InstantiateRoomObject(GlobalSettings.PickupsPath + pickupPrefabs[y].name, position, Quaternion.identity);
+            }
+        }
+            
     }
 }
