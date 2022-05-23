7144817aa663fc37457ef18ccae67ba06941d2df
gretgurt <gretahassler@hotmail.com>
2022-05-23 19:46:20 +0200

    Tried making enemy health animation

--- Assets/Scripts/Handlers/EnemyHealthHandler.cs
+++ Assets/Scripts/Handlers/EnemyHealthHandler.cs
@@ -12,11 +12,27 @@ public class EnemyHealthHandler : HealthHandler
     [SerializeField] private GameObject hitVFX;
     GameObject vfx;
 
+    private bool hasBeenHurt = false;
+    private Animator anim;
+    
+
     public override void TakeDamage(int amount)
     {
         photonView.RPC(nameof(TakeDamageRPC), RpcTarget.All, amount);
         // Plays VFX where the bullet hits on enemy
         Destroy(Instantiate(hitVFX,transform.position, transform.rotation), 2f);
+        /*if (!hasBeenHurt)
+        {
+            hasBeenHurt = true;
+            anim = GetComponentInChildren<Animator>();
+            if(anim != null)
+            {
+                //hurtAnimator.SetTrigger("Hurt");
+                anim = GetComponentInChildren<Animator>();
+                anim.SetTrigger("Hurt");
+                //gameObject.GetComponentInChildren<EnemyHurtAnimation>().PlayEnemyHurtAnim();
+            }
+        }*/
     }
 
     [PunRPC]
