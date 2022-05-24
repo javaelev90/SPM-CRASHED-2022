2f6af3390d074bee507d6e752083290b1ed691e0
Simon Täcklind <simon.tacklind@gmail.com>
2022-05-24 11:15:40 +0200

    Added Labyrinth material and re-enabled meshcombining.
    Also changed Game Settings to use SpelTest2 as game scene

--- Assets/Scripts/Labyrinth/LabyrinthReader.cs
+++ Assets/Scripts/Labyrinth/LabyrinthReader.cs
@@ -7,7 +7,7 @@ public class LabyrinthReader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //CombineMeshes();
+        CombineMeshes();
         //GenerateCubePosition2DArray();
     }
 
